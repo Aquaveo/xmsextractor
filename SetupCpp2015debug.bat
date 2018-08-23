3979a4f6ace6a05bd0cbee329d1f68dcbb151f09
CD <cdickson@aquaveo.com>
2018-08-23 10:32:11 -0600

    Resolve python line difference with xmscore in CMakeLists.txt by removing them. Fix missing xmscore complaints by removing it and others from xmsprofile_debug. Set xmsprofile_debug XMS to false. (The Python setup will need xmsprofile_debug XMS set to false, testing to false, and python to true.)

--- SetupCpp2015debug.bat
+++ SetupCpp2015debug.bat
@@ -1,7 +1,8 @@
 mkdir build
 pushd .
 cd build
-REM goto skip
+
+goto skip
 echo Remove these 3 --build lines once they are no longer needed.
 conan install --build xmscore -pr ..\dev\xmsprofile_debug ..
 conan install --build xmsgrid -pr ..\dev\xmsprofile_debug ..
@@ -10,6 +11,7 @@ conan install --build xmsgrid -pr ..\dev\xmsprofile_debug ..
 conan install --build xmscore -pr ..\dev\xmsprofile_debug ..
 pause
 :skip
+
 conan install -pr ..\dev\xmsprofile_debug ..
 pause
 cmake .. -G "Visual Studio 14 2015 Win64" -DIS_PYTHON_BUILD=False -DBUILD_TESTING=True
