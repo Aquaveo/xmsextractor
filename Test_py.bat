3fad9c4b2ae80ccf7d2a085aed640d2d5422a016
CD <cdickson@aquaveo.com>
2018-08-24 14:33:34 -0600

    Fixed Gage pull review: move the scripts until we can design how we want them to go. Also the module doc is incorrect. Still refers to grid in xmsextractor/python/xmsextractor_py.cpp

--- Test_py.bat
+++
@@ -1,9 +0,0 @@
-pip install numpy
-pushd .
-cd build_py\lib
-python -m unittest discover -v -p *_pyt.py -s ../../xmsextractor/python/extractor
-popd
-pause
-echo must copy libraries to lib folder:
-echo xmscore_py.cp35-win_amd64.pyd
-echo xmsextractor_py.cp35-win_amd64.pyd
