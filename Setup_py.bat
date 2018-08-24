bb112418a7d570a9038af53df0c209523b7d2070
CD <cdickson@aquaveo.com>
2018-08-24 08:18:15 -0600

    Add testing instructions to Setup_py.bat.

--- Setup_py.bat
+++ Setup_py.bat
@@ -6,3 +6,9 @@ pause
 cmake .. -G"Visual Studio 14 2015 Win64" -DIS_PYTHON_BUILD=True -DBUILD_TESTING=False -DPYTHON_TARGET_VERSION=3.5
 pause
 popd
+echo off
+echo OPEN VS2015 FILE build_py\xmsextractor.sln
+echo BUILD PROJECT xmsextractor_py FOR RELEASE x64
+echo COPY PYTHON LIBS FROM C:\Users\<account>\.conan\data\xmsgrid\...\*.pyd TO build_py\lib\.
+echo RUN TEST FILE Test_py.bat
+pause
