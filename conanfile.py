96046e1c4cfe27baffb1b46e7d7ee0a8b41ee574
Gage Larsen <glarsen@aquaveo.com>
2018-10-08 14:39:49 -0600

    use new test_path convention

--- conanfile.py
+++ conanfile.py
@@ -86,7 +86,7 @@ class XmsextractorConan(ConanFile):
         cmake.definitions["XMS_VERSION"] = '{}'.format(self.version)
         cmake.definitions["IS_PYTHON_BUILD"] = self.options.pybind
         cmake.definitions["BUILD_TESTING"] = self.options.testing
-        cmake.definitions["XMSEXTRACTOR_TEST_PATH"] = "test_files"
+        cmake.definitions["XMS_TEST_PATH"] = "test_files"
         cmake.definitions["PYTHON_TARGET_VERSION"] = self.env.get("PYTHON_TARGET_VERSION", "3.6")
         cmake.configure(source_folder=".")
         cmake.build()
