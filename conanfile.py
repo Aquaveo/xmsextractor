a9602ee41a756c2d66128b8845bc6f874a77eb2a
glarsen <glarsen@aquaveo.com>
2018-12-04 11:54:02 -0700

    fix python naming typo

--- conanfile.py
+++ conanfile.py
@@ -68,9 +68,9 @@ class XmsextractorConan(ConanFile):
             self.requires("pybind11/2.2.2@aquaveo/stable")
 
         # Use the dev version of XMSCore, XMSInterp, and XMSGrid
-        self.requires("xmscore/[>=1.0.43,<2.0.0]@aquaveo/stable")
-        self.requires("xmsinterp/[>=1.0.23,<2.0.0]@aquaveo/stable")
-        self.requires("xmsgrid/[>=1.0.13,<2.0.0]@aquaveo/stable")
+        self.requires("xmscore/[>=2.0.1,<3.0.0]@aquaveo/stable")
+        self.requires("xmsinterp/[>=2.0.0,<3.0.0]@aquaveo/stable")
+        self.requires("xmsgrid/[>=2.0.0,<3.0.0]@aquaveo/stable")
 
     def build(self):
         cmake = CMake(self)
