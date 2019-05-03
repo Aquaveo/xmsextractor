ab888705617d486e3a25c6d6051899b6ac5cad76
gagelarsen <glarsen@aquaveo.com>
2019-05-03 09:08:32 -0600

    doc strings for two extractor classes

--- _package/xmsextractor/extractor/ugrid_2d_polyline_data_extractor.py
+++ _package/xmsextractor/extractor/ugrid_2d_polyline_data_extractor.py
@@ -1,3 +1,12 @@
+"""
+********************************************************************************
+* Name: interp_anisotropic.py
+* Author: Gage Larsen, Matt LeBaron
+* Created On: May 2nd, 2019
+* Copyright: (c)
+* License: BSD 2-Clause
+********************************************************************************
+"""
 from .._xmsextractor import extractor
 
 
@@ -28,25 +37,58 @@ class UGrid2dPolylineDataExtractor(object):
             ))
 
     def set_grid_scalars(self, scalars, activity, scalar_location):
+        """
+        Setup cell scalars to be used to extract interpolated data.
+
+        Args:
+            scalars (iterable): The cell scalars.
+            activity (iterable): The activity of the cells.
+            scalar_location (string): The location at which the data is currently stored. One of 'points', 'cells',
+                or 'unknown'
+        """
         self._check_data_locations(scalar_location)
         data_location = self.data_locations[scalar_location]
         self._instance.SetGridScalars(scalars, activity, data_location)
 
     def set_polyline(self, polyline):
+        """
+        Set the polyline along which to extract the scalar data. Locations
+        crossing cell boundaries are computed along the polyline.
+
+        Args:
+            polyline (iterable): The polyline.
+        """
         self._instance.SetPolyline(polyline)
 
     def extract_data(self):
+        """
+        Extract interpolated data for the previously set locations.
+
+        Returns:
+            The interpolated scalars.
+        """
         return self._instance.ExtractData()
 
     def compute_locations_and_extract_data(self, polyline):
+        """
+        Extract data for given polyline.
+
+        Args:
+            polyline (iterable): The polyline.
+
+        Returns:
+            A tuple of the extracted data and their locations
+        """
         return self._instance.ComputeLocationsAndExtractLocations(polyline)
 
     @property
     def extract_locations(self):
+        """Locations of points to extract interpolated scalar data from."""
         return self._instance.GetExtractLocations()
 
     @property
     def use_idw_for_point_data(self):
+        """Use IDW to calculate point scalar values from cell scalars."""
         return self._instance.GetUseIdwForPointData()
 
     @use_idw_for_point_data.setter
@@ -55,6 +97,7 @@ class UGrid2dPolylineDataExtractor(object):
 
     @property
     def no_data_value(self):
+        """Value to use when extracted value is in inactive cell or doesn't intersect with the grid."""
         return self._instance.GetNoDataValue()
 
     @no_data_value.setter
