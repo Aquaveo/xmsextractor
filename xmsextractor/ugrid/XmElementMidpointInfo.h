3949fc69bcb50abffcaf47b63440934fb578984f
csmemoe <csmemoe@aquaveo.com>
2022-02-08 13:24:42 -0700

    Add options to triangulate elements using midpoints.

--- xmsextractor/ugrid/XmElementMidpointInfo.h
+++ xmsextractor/ugrid/XmElementMidpointInfo.h
@@ -12,15 +12,8 @@
 // 3. Standard library headers
 
 // 4. External library headers
-#include <boost/container/flat_map.hpp>
-
-#include <xmscore/misc/base_macros.h>   // XM_DISALLOW_COPY_AND_ASSIGN
-#include <xmscore/misc/boost_defines.h> // BSHP
-#include <xmscore/stl/vector.h>         // VecInt
 
 // 5. Shared code headers
-#include <xmsextractor/ugrid/XmElementEdge.h>
-#include <xmsextractor/ugrid/XmElementMidpointInfo.h>
 
 //----- Forward declarations ---------------------------------------------------
 
@@ -30,32 +23,14 @@
 namespace xms
 {
 //----- Forward declarations ---------------------------------------------------
-typedef boost::container::flat_map<XmElementEdge, XmElementMidpointInfo> FlatMapEdgeMidpointInfo;
 
 //----- Constants / Enumerations -----------------------------------------------
 
 //----- Structs / Classes ------------------------------------------------------
 ////////////////////////////////////////////////////////////////////////////////
-class XmUGridTriangulatorBase
+struct XmElementMidpointInfo
 {
-public:
-  XmUGridTriangulatorBase();
-  virtual ~XmUGridTriangulatorBase();
-  virtual int AddCentroidPoint(const int a_cellIdx, const Pt3d& a_pt) = 0;
-  virtual void AddTriangle(const int a_cellIdx,
-                           const int a_pt1,
-                           const int a_pt2,
-                           const int a_pt3) = 0;
-  virtual const VecPt3d& GetPoints() const = 0;
-  void SetMidpoints(BSHP<FlatMapEdgeMidpointInfo> a_midPoints);
-  XmElementMidpointInfo& FindMidPoint(const XmElementEdge& a_edge);
-  bool GenerateCentroidTriangles(int a_cellIdx, const VecInt& a_cellPointIdxs);
-  void BuildEarcutTriangles(int a_cellIdx, const VecInt& a_cellPointIdxs);
-
-private:
-  XM_DISALLOW_COPY_AND_ASSIGN(XmUGridTriangulatorBase)
-  class impl;
-  BSCP<impl> m_impl;
+  long cellId, midPtId;
 };
 
 //----- Function prototypes ----------------------------------------------------
