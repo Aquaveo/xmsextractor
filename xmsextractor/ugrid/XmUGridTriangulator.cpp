6e8220b1ec32cde54ffa8b2a2fa0efb88db99dce
csmemoe <csmemoe@aquaveo.com>
2022-02-08 13:24:42 -0700

    Add options to triangulate elements using midpoints.

--- xmsextractor/ugrid/XmUGridTriangulator.cpp
+++ xmsextractor/ugrid/XmUGridTriangulator.cpp
@@ -57,9 +57,12 @@ public:
   virtual int GetNumTriangles() const final;
   virtual int GetCellFromTriangle(const int a_triangleIdx) const final;
   virtual int GetCellCentroid(int a_cellIdx) const final;
+  virtual void InitMidpoints() final;
+  virtual int AddMidPoint(const int a_cellIdx, const int a_ptIdx0, const int a_ptIdx1) final;
 
 private:
   XmUGridTriangulatorImpl();
+  const XmUGrid& m_ugrid;
   BSHP<VecPt3d> m_points;     ///< Triangle points for the UGrid
   BSHP<VecInt> m_triangles;   ///< Triangles for the UGrid
   VecInt m_centroidIdxs;      ///< Index of each cell centroid or -1 if none
@@ -75,7 +78,8 @@ private:
 /// \brief Constructor
 //------------------------------------------------------------------------------
 XmUGridTriangulatorImpl::XmUGridTriangulatorImpl(const XmUGrid& a_ugrid)
-: m_points(new VecPt3d(a_ugrid.GetLocations()))
+: m_ugrid(a_ugrid)
+, m_points(new VecPt3d(a_ugrid.GetLocations()))
 , m_triangles(new VecInt)
 , m_centroidIdxs(a_ugrid.GetCellCount(), -1)
 , m_triangleToCellIdx()
@@ -123,6 +127,53 @@ int XmUGridTriangulatorImpl::GetCellCentroid(int a_cellIdx) const
     pointIdx = m_centroidIdxs[a_cellIdx];
   return pointIdx;
 } // XmUGridTriangulatorImpl::GetCellCentroid
+//------------------------------------------------------------------------------
+/// \brief Sets the option to create midpoints and sets up the required data.
+//------------------------------------------------------------------------------
+void XmUGridTriangulatorImpl::InitMidpoints()
+{
+  BSHP<FlatMapEdgeMidpointInfo> midPoints(new FlatMapEdgeMidpointInfo());
+  VecInt attachedPts;
+  int numPts = (int)m_points->size();
+  for (int ii = 0; ii < numPts; ++ii)
+  {
+    m_ugrid.GetPointAdjacentPoints(ii, attachedPts);
+    if (!attachedPts.empty())
+    {
+      XmElementMidpointInfo info = {-1, -1};
+      for (auto&& ptIdx : attachedPts)
+        midPoints->insert(midPoints->end(), std::pair<XmElementEdge, XmElementMidpointInfo>(
+                                              XmElementEdge(ii, ptIdx), info));
+    }
+  }
+  SetMidpoints(midPoints);
+} // XmUGridTriangulatorImpl::InitMidpoints
+//------------------------------------------------------------------------------
+/// \brief Add a cell point at the midpoint between the given point indices.
+/// \param a_cellIdx The cell index for the centroid point
+/// \param a_ptIdx0 The index of the first cell point
+/// \param a_ptIdx1 The index of the second cell point
+/// \return The index of the added point
+//------------------------------------------------------------------------------
+int XmUGridTriangulatorImpl::AddMidPoint(const int a_cellIdx,
+                                         const int a_ptIdx0,
+                                         const int a_ptIdx1)
+{
+  XmElementEdge edgePair(a_ptIdx0, a_ptIdx1);
+  XmElementMidpointInfo& info = FindMidPoint(XmElementEdge(a_ptIdx0, a_ptIdx1));
+  auto&& cellMid = info.cellId;
+  auto&& idMid = info.midPtId;
+  if (idMid < 0)
+  {
+    Pt3d p0 = m_points->at(a_ptIdx0);
+    Pt3d p1 = m_points->at(a_ptIdx1);
+    double midPt[3] = {(p0[0] + p1[0]) / 2.0, (p0[1] + p1[1]) / 2.0, (p0[2] + p1[2]) / 2.0};
+    cellMid = a_cellIdx;
+    idMid = (long)m_points->size();
+    m_points->emplace_back(midPt[0], midPt[1], midPt[2]);
+  }
+  return (int)idMid;
+} // XmUGridTriangulatorImpl::AddMidPoint
 //------------------------------------------------------------------------------
 /// \brief Get the generated triangle points.
 /// \return The triangle points
