fc07538dbbeb0aa3e4ada43ff0290e25c0c59bab
Bill Dolinar <bill_dolinar@yahoo.com>
2022-10-17 15:04:11 -0600

    Changed to gcc 7 builds.

--- _package/xms/extractor/__init__.py
+++ _package/xms/extractor/__init__.py
@@ -2,4 +2,8 @@
 from .ugrid_2d_data_extractor import UGrid2dDataExtractor  # NOQA: F401
 from .ugrid_2d_polyline_data_extractor import UGrid2dPolylineDataExtractor  # NOQA: F401
 
+<<<<<<< HEAD
 __version__ = '9.0.5'
+=======
+__version__ = '8.0.2'
+>>>>>>> df21917 (Changed to gcc 7 builds.)
